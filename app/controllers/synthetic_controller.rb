48af723c80e9aa44864ffd05f1ae3856330978f0
jia-l <jia-l@b8adeac4-d2c7-4471-b17a-50071c40be99>
2008-10-24 10:00:40 +0000

    git-svn-id: http://dahlia.naist.jp/svn-repo/Cradle/trunk@40 b8adeac4-d2c7-4471-b17a-50071c40be99

--- app/controllers/synthetic_controller.rb
+++ app/controllers/synthetic_controller.rb
@@ -5,14 +5,10 @@ class SyntheticController < ApplicationController
     lexeme_class = verify_domain(params[:info][:domain])['Lexeme']
     case params[:split_action][:type]
       when "define"
-        if params[:info][:from] == "creation"
-          structure = [params[:info][:original_id], 'initial_'+lexeme_class.constantize.find(params[:info][:original_id].to_i).surface]
-        elsif params[:info][:from] == "modification"
-          structure = get_structure(:ref_id=>params[:info][:original_id], :synthetic_class=>verify_domain(params[:info][:domain])['Synthetic'])
-        end
+	structure = make_initial_structure(:info=>params[:info])
       when "delete"
+        params[:structure] = swap_structure_array_and_string(params[:structure].dup, [])
         if params[:split_action][:divide_type] == "vertical"
-          params[:structure] = swap_structure_array_and_string(params[:structure].dup, [])
           if params[:split_action][:point].blank?
             params[:structure] = [params[:info][:original_id], 'initial_'+lexeme_class.constantize.find(params[:info][:original_id].to_i).surface]
           else
@@ -24,63 +20,49 @@ class SyntheticController < ApplicationController
           end
         elsif params[:split_action][:divide_type] == "horizontal"
           if params[:left].blank? or params[:right].blank?
-            case params[:info][:domain]
-              when 'jp'
-                flash.now[:notice_err] = '<ul><li>左右の部分を両方選択してください！</li></ul>'
-              when 'cn'
-                flash.now[:notice_err] = '<ul><li>左右两个部分必须都要选择！</li></ul>'
-              when 'en'
-                flash.now[:notice_err] = '<ul><li>Plese select both left and right part in division!</li></ul>'
-            end
+	    flash.now[:notice_err] = get_error_message(params[:info][:domain])
           else
-            eval('params[:structure]'+params[:split_action][:left_hand_index]+'=transfer_left_or_right_part_to_structure(params[:left], lexeme_class)')
             eval('params[:structure]'+params[:split_action][:right_hand_index]+'=transfer_left_or_right_part_to_structure(params[:right], lexeme_class)')
+            eval('params[:structure]'+params[:split_action][:left_hand_index]+'=transfer_left_or_right_part_to_structure(params[:left], lexeme_class)')
           end
         end
         structure = params[:structure]
       else
+        params[:structure] = swap_structure_array_and_string(params[:structure].dup, [])
         if params[:left].blank? or params[:right].blank?
-          case params[:info][:domain]
-            when 'jp'
-              flash.now[:notice_err] = '<ul><li>左右の部分を両方選択してください！</li></ul>'
-            when 'cn'
-              flash.now[:notice_err] = '<ul><li>左右两个部分必须都要选择！</li></ul>'
-            when 'en'
-              flash.now[:notice_err] = '<ul><li>Plese select both left and right part in division!</li></ul>'
+	  flash.now[:notice_err] = get_error_message(params[:info][:domain])
+        elsif params[:split_action][:type] == 'new'
+          if params[:split_action][:divide_type] == "vertical"
+            if eval('params[:structure]'+params[:split_action][:point]) =~ /^meta_(.*)$/
+              temp = ['meta_'+$1]
+            elsif eval('params[:structure]'+params[:split_action][:point]) =~ /^update_(.*)$/
+	      temp = ['update_'+$1]
+            else
+              temp = [eval('params[:structure]'+params[:split_action][:point])]
+            end
+            temp[1] = transfer_left_or_right_part_to_structure(params[:left], lexeme_class)
+            temp[2] = transfer_left_or_right_part_to_structure(params[:right], lexeme_class)
+            eval('params[:structure]'+params[:split_action][:point]+'=temp')
+          elsif params[:split_action][:divide_type] == "horizontal"
+            temp = transfer_left_or_right_part_to_structure(params[:right], lexeme_class)
+            eval('params[:structure]'+params[:split_action][:point]+'=temp')
+            temp = params[:split_action][:point].split('][')
+            if temp.size == 1
+              params[:structure].insert(temp[0][1..-2].to_i, transfer_left_or_right_part_to_structure(params[:left], lexeme_class))
+            else
+              pre_index = temp[0..-2].join('][')+']'
+              index = temp.last[0..-2].to_i
+              temp = transfer_left_or_right_part_to_structure(params[:left], lexeme_class)
+              eval('params[:structure]'+pre_index+'.insert(index, temp)')
+            end
           end
-          structure = params[:structure]
-        else
-          case params[:split_action][:type]
-            when "new"
-              if params[:split_action][:divide_type] == "vertical"
-                if eval('params[:structure]'+params[:split_action][:point]) =~ /^meta_(.*)$/
-                  temp = ['meta_'+$1]
-                else
-                  temp = [eval('params[:structure]'+params[:split_action][:point])]
-                end
-                temp[1] = transfer_left_or_right_part_to_structure(params[:left], lexeme_class)
-                temp[2] = transfer_left_or_right_part_to_structure(params[:right], lexeme_class)
-                eval('params[:structure]'+params[:split_action][:point]+'=temp')
-              elsif params[:split_action][:divide_type] == "horizontal"
-                temp = transfer_left_or_right_part_to_structure(params[:right], lexeme_class)
-                eval('params[:structure]'+params[:split_action][:point]+'=temp')
-                temp = params[:split_action][:point].split('][')
-                if temp.size == 1
-                  params[:structure].insert(temp[0][1..-2].to_i, transfer_left_or_right_part_to_structure(params[:left], lexeme_class))
-                else
-                  pre_index = temp[0..-2].join('][')+']'
-                  index = temp.last[0..-2].to_i
-                  temp = transfer_left_or_right_part_to_structure(params[:left], lexeme_class)
-                  eval('params[:structure]'+pre_index+'.insert(index, temp)')
-                end
-              end
-            when "modify"
-              eval('params[:structure]'+params[:split_action][:left_hand_index]+'=transfer_left_or_right_part_to_structure(params[:left], lexeme_class)')
-              eval('params[:structure]'+params[:split_action][:right_hand_index]+'=transfer_left_or_right_part_to_structure(params[:right], lexeme_class)')
-          end
-          structure = params[:structure]
+	elsif params[:split_action][:type] ==  "modify"
+          eval('params[:structure]'+params[:split_action][:left_hand_index]+'=transfer_left_or_right_part_to_structure(params[:left], lexeme_class)')
+          eval('params[:structure]'+params[:split_action][:right_hand_index]+'=transfer_left_or_right_part_to_structure(params[:right], lexeme_class)')
         end
+        structure = params[:structure]
     end
+	debugger
     render :update do |page|
       page["synthetic_struct"].replace :partial=>"show_internal_structure", :object=> structure, :locals=>{:info=>params[:info]}
     end
@@ -93,23 +75,15 @@ class SyntheticController < ApplicationController
     lexemes_right = lexeme_class.constantize.find(:all, :include=>[:struct], :conditions=>["surface=?", params[:right]], :order=>"id ASC")
     case params[:split_action][:type]
       when "modify"
-        left_id = eval('params[:structure]'+params[:split_action][:left_hand_index])
-        right_id = eval('params[:structure]'+params[:split_action][:right_hand_index])
-        left_id = "meta" if is_string(left_id) == false
-        right_id = "meta" if is_string(right_id) == false
-        left_id = "dummy" if left_id =~ /^dummy_(.*)$/
-        right_id = "dummy" if right_id =~ /^dummy_(.*)$/
+	left_id = get_type_or_id_from_structure(eval('params[:structure]'+params[:split_action][:left_hand_index]))
+	right_id = get_type_or_id_from_structure(eval('params[:structure]'+params[:split_action][:right_hand_index]))
       when "delete"
         if params[:split_action][:left_hand_index].include?('..')
           left_id = nil
-          right_id = eval('params[:structure]'+params[:split_action][:right_hand_index])
-          right_id = "meta" if is_string(right_id) == false
-          right_id = "dummy" if right_id =~ /^dummy_(.*)$/
+	  right_id = get_type_or_id_from_structure(eval('params[:structure]'+params[:split_action][:right_hand_index]))
         elsif params[:split_action][:left_hand_index].include?('..')
           right_id = nil
-          left_id = eval('params[:structure]'+params[:split_action][:left_hand_index])
-          left_id = "meta" if is_string(left_id) == false
-          left_id = "dummy" if left_id =~ /^dummy_(.*)$/
+	  left_id = get_type_or_id_from_structure(eval('params[:structure]'+params[:split_action][:left_hand_index]))
         end
       when "new"
         left_id = nil
@@ -152,26 +126,38 @@ class SyntheticController < ApplicationController
 #    end
 #  end
 
-
-
-
-
   private
-  def get_structure(option)
-    option[:meta_id] = 0 if option[:meta_id].blank?
-    temp_synthetic = option[:synthetic_class].constantize.find(:first, :conditions=>["sth_ref_id=? and sth_meta_id=?", option[:ref_id].to_i, option[:meta_id]])
-    option[:meta_id] ==0 ? structure = [option[:ref_id]] : structure = ['meta_'+temp_synthetic.sth_surface]
-    temp_array = temp_synthetic.sth_struct.map{|item| item.delete('-')}
-    temp_array.each{|item|
-      if item =~ /^\d+$/
-        structure << item
-      elsif item =~ /^meta_(\d+)$/
-        structure << get_structure(:ref_id=>option[:ref_id], :meta_id=>$1.to_i, :synthetic_class=>option[:synthetic_class])
-      end
-    }
-    return structure
+  def make_initial_structure(option)
+    if option[:info][:from] == 'creation'
+      return [option[:info][:original_id], 'initial_'+verify_domain(option[:info][:domain])['Lexeme'].constantize.find(option[:info][:original_id].to_i).surface]
+    elsif option[:info][:from] == 'modification'
+      option[:meta_id] = 0 if option[:meta_id].blank?
+      temp_synthetic = verify_domain(option[:info][:domain])['Synthetic'].constantize.find(:first, :conditions=>["sth_ref_id=? and sth_meta_id=?", 
+option[:ref_id].to_i, option[:meta_id]])
+      option[:meta_id] ==0 ? structure = [option[:info][:original_id]] : structure = ['meta_'+temp_synthetic.sth_surface]
+      temp_array = temp_synthetic.sth_struct.map{|item| item.delete('-')}
+      temp_array.each{|item|
+        if item =~ /^\d+$/
+          structure << item
+        elsif item =~ /^meta_(\d+)$/
+          structure << make_initial_structure(:info=>option[:info], :meta_id=>$1.to_i)
+        end
+      }
+      return structure      
+    end
   end
-  
+
+  def get_error_message(domain)
+    case domain
+      when 'jp'
+        return '<ul><li>左右の部分を両方選択してください！</li></ul>'
+      when 'cn'
+        return '<ul><li>左右两个部分必须都要选择！</li></ul>'
+      when 'en'
+        return '<ul><li>Plese select both left and right part in division!</li></ul>'
+    end
+  end
+
   def transfer_left_or_right_part_to_structure(option, lexeme_class)
     type, value = option.split(',')
     case type
@@ -182,8 +168,25 @@ class SyntheticController < ApplicationController
       when 'select'
         return value
       when 'update'
-        return [value, 'chars_'+lexeme_class.constantize.find(value.to_i).surface]
+        return 'update_'+value
     end
   end
-   
+
+  def get_type_or_id_from_structure(item)
+    if is_string(item) == false
+      if item[0] =~ /^meta_(.*)$/
+	return 'meta'
+      else
+	return item[0]
+      end
+    else
+      if item =~ /^meta_(.*)$/
+        return 'meta'
+      elsif item =~ /^dummy_(.*)$/
+        return 'dummy'
+      else
+        return item
+      end	
+    end
+  end
 end
--- app/helpers/synthetic_helper.rb
+++ app/helpers/synthetic_helper.rb
@@ -2,7 +2,7 @@ module SyntheticHelper
   include ApplicationHelper
 
   def show_internal_structure(option)
-    option[:original_strcut] = swap_structure_array_and_string("", option[:structure].dup) if option[:original_strcut].blank?
+    option[:original_struct] = swap_structure_array_and_string("", option[:structure].dup) if option[:original_struct].blank?
     option[:section_indexes] = "" if option[:section_indexes].blank?
     html_string = ""
     option[:structure].each_with_index{|section,index|
@@ -19,7 +19,13 @@ module SyntheticHelper
           html_string << $1+"\n"
           html_string << "</td>\n"
         else
-          ((section =~ /^meta_(.*)$/) or (section =~ /^initial_(.*)$/)) ? temp = $1.scan(/./) : temp = verify_domain(option[:info][:domain])['Lexeme'].constantize.find(section.to_i).surface.scan(/./)
+	  if (section =~ /^meta_(.*)$/) or (section =~ /^initial_(.*)$/)
+	    temp = $1.scan(/./)
+	  elsif section =~ /^update_(.*)$/
+	    temp = verify_domain(option[:info][:domain])['Lexeme'].constantize.find($1.to_i).surface.scan(/./)
+	  else
+	    temp = verify_domain(option[:info][:domain])['Lexeme'].constantize.find(section.to_i).surface.scan(/./)
+	  end
           split_action = {:type=>"new", :point=>option[:section_indexes]+'['+index.to_s+']'}
           for inner_index in 0..temp.size-1
             html_string << "<td style='background:#F0F8FF;padding:5px 10px 5px 10px;color:#047;font-weight:bold;text-align:center;font-size:120%;'>\n"
@@ -32,22 +38,22 @@ module SyntheticHelper
               if option[:first_time]==true
                 html_string << link_to_remote(image_tag("internal.jpg", :border=>0), :url=>{:action=>"split_word", :left=>left,
                                                                                             :right=>right,         :split_action=>split_action.update(:divide_type=>"horizontal"),
-                                                                                            :info=>option[:info],  :structure=>option[:original_strcut]})
+                                                                                            :info=>option[:info],  :structure=>option[:original_struct]})
               elsif option[:first_time]==false
                 html_string << link_to_remote(image_tag("internal-horizontal.jpg", :border=>0), :url=>{:action=>"split_word", :left=>left,
                                                                                                        :right=>right,         :split_action=>split_action.update(:divide_type=>"horizontal"),
-                                                                                                       :info=>option[:info],  :structure=>option[:original_strcut]})
+                                                                                                       :info=>option[:info],  :structure=>option[:original_struct]})
                 html_string << '<br/>'
                 html_string << link_to_remote(image_tag("internal-vertical.jpg", :border=>0), :url=>{:action=>"split_word", :left=>left,
                                                                                                      :right=>right,         :split_action=>split_action.update(:divide_type=>"vertical"),
-                                                                                                     :info=>option[:info],  :structure=>option[:original_strcut]})
+                                                                                                     :info=>option[:info],  :structure=>option[:original_struct]})
               end
               html_string << "</td>\n"
             end
           end
         end
       else
-        html_string << show_internal_structure(:structure=>section, :first_time=>false, :info=>option[:info], :original_strcut=>option[:original_strcut],
+        html_string << show_internal_structure(:structure=>section, :first_time=>false, :info=>option[:info], :original_struct=>option[:original_struct],
                                                :section_indexes=>option[:section_indexes]+'['+index.to_s+']')
       end
       unless index == option[:structure].size-1
@@ -56,7 +62,7 @@ module SyntheticHelper
         right = get_chars_from_structure(option[:structure][index+1], option[:info][:domain])
         option[:structure].size > 3 ? divide_type = "horizontal" : divide_type = "vertical"
         html_string << link_to_remote(image_tag("internal-plus.jpg", :border=>0, :style=>"vertical-align: middle;"),
-                                      :url=>{:action=>"split_word", :structure=>option[:original_strcut],
+                                      :url=>{:action=>"split_word", :structure=>option[:original_struct],
                                       :left=>left,                  :right=>right,
                                       :info=>option[:info],         :split_action=>{:type=>"modify", :divide_type=>divide_type,
                                                                                     :left_hand_index=>(option[:section_indexes]+'['+index.to_s+']'),
@@ -75,12 +81,12 @@ module SyntheticHelper
             right_hand_index = option[:section_indexes]+'['+(index+2).to_s+']'
           end
           split_action = {:type=>"delete", :divide_type=>divide_type, :left_hand_index=>left_hand_index, :right_hand_index=>right_hand_index}
-          html_string << link_to_remote(image_tag("internal-minus.jpg", :border=>0, :style=>"vertical-align: middle;"), :url=>{:action=>"split_word", :structure=>option[:original_strcut],
+          html_string << link_to_remote(image_tag("internal-minus.jpg", :border=>0, :style=>"vertical-align: middle;"), :url=>{:action=>"split_word", :structure=>option[:original_struct],
                                                                                                                                :info=>option[:info],  :split_action=>split_action,
                                                                                                                                :left=>left,           :right=>right})          
         elsif divide_type == "vertical"
           html_string << link_to_remote(image_tag("internal-minus.jpg", :border=>0, :style=>"vertical-align: middle;"),
-                                        :url=>{:action=>"define_internal_structure", :structure=>option[:original_strcut],
+                                        :url=>{:action=>"define_internal_structure", :structure=>option[:original_struct],
                                                :info=>option[:info],                 :split_action=>{:type=>"delete", :divide_type=>divide_type, :point=>option[:section_indexes]}})
         end
         html_string << "</td>\n"
@@ -89,41 +95,72 @@ module SyntheticHelper
     return html_string
   end
   
-  def get_showing_string(option)
+  def get_structure_display_string(option)
     lexeme_class = verify_domain(option[:domain])['Lexeme']
+    option[:top_level]="true" if option[:top_level].blank?
     char_string = []
-    option[:structure].each_with_index{|item, index|
-      next if index == 0
-      if is_string(item) == true
-        if (item =~ /^meta_(.*)$/) or (item =~ /^initial_(.*)$/)
-          char_string << $1+'()'
-        elsif item =~ /^dummy_(.*)$/
-          char_string << $1+'(dummy)'
+    if is_string(option[:structure]) == true and option[:structure] =~ /^update_(.*)$/
+      temp = $1
+      return lexeme_class.constantize.find(temp.to_i).surface+'('+temp+')'+'  ==>  []'
+    else
+      option[:structure].each_with_index{|item, index|
+        next if index == 0
+        if is_string(item) == true
+          if (item =~ /^meta_(.*)$/) or (item =~ /^initial_(.*)$/)
+            char_string << $1+'()'
+	  elsif item =~ /^update_(.*)$/
+	    char_string << lexeme_class.constantize.find($1.to_i).surface+'('+$1+')'
+          elsif item =~ /^dummy_(.*)$/
+            char_string << $1+'(dummy)'
+          else
+            char_string << lexeme_class.constantize.find(item.to_i).surface+'('+item+')'
+          end
         else
-          char_string << lexeme_class.constantize.find(item.to_i).surface+'('+item+')'
+	  if item[0] =~ /^meta_(.*)$/
+	    char_string << get_structure_display_string(:structure=>item, :domain=>option[:domain], :top_level=>"false")
+	  elsif item[0] =~ /^update_(.*)$/
+	    char_string << lexeme_class.constantize.find($1.to_i).surface+'('+$1+')'
+	  elsif item[0] =~ /^\d+$/
+	    char_string << lexeme_class.constantize.find(item[0].to_i).surface+'('+item[0]+')'
+	  end
         end
+      }
+      if option[:top_level]=="false"
+        return '[ '+char_string.join(',   ')+' ]'
       else
-        char_string << get_showing_string(:structure=>item, :domain=>option[:domain])
+        if option[:structure][0] =~ /^\d+$/
+          root = lexeme_class.constantize.find(option[:structure][0].to_i).surface+'('+option[:structure][0]+')'
+        elsif option[:structure][0] =~ /^update_(.*)$/
+          id = $1
+          root = lexeme_class.constantize.find(id.to_i).surface+'('+id+')'
+        end
+        return root+'  ===>  '+'[ '+char_string.join(',   ')+' ]'
       end
-    }
-    return '[  '+char_string.join(',')+'  ]'
+    end
   end
-  
+
+  def get_create_and_update_structures(option)
+    create_indexes, update_indexes = find_structure_indexes(:structure=>option[:structure])
+    create_array = []
+    update_array = []
+    create_indexes.each{|index_string| create_array << get_structure_display_string(:structure=>eval('option[:structure]'+index_string), :domain=>option[:domain])}
+    update_indexes.each{|index_string| update_array << get_structure_display_string(:structure=>eval('option[:structure]'+index_string), :domain=>option[:domain])}
+    return create_array, update_array
+  end
+
   private
   def get_chars_from_structure(section, domain)
     char_string = ""
     if is_string(section) == true
       if section =~ /^\d+$/
-        char_string << verify_domain(domain)['Lexeme'].constantize.find(section).surface
-      elsif section =~ /^dummy_(.*)$/
+        char_string << verify_domain(domain)['Lexeme'].constantize.find(section.to_i).surface
+      elsif (section =~ /^dummy_(.*)$/) or (section =~ /^meta_(.*)$/)
         char_string << $1
+      elsif section =~ /^update_(.*)$/
+	char_string << verify_domain(domain)['Lexeme'].constantize.find($1.to_i).surface
       end
     else
-      if section[0] =~ /^\d+$/
-        char_string << verify_domain(domain)['Lexeme'].constantize.find(section[0]).surface
-      else
-        section[1..-1].each{|item| char_string << get_chars_from_structure(item, domain)}
-      end
+      section[1..-1].each{|item| char_string << get_chars_from_structure(item, domain)}
     end
     return char_string
   end
@@ -140,10 +177,40 @@ module SyntheticHelper
       return array
     end
     unless array.blank?
+      string = []
       for index in 0..array.size-1
-        array[index] = swap_structure_array_and_string("", array[index], step+1) if is_string(array[index]) == false
+	if is_string(array[index]) == true
+	  string << array[index]
+	else
+	  string << swap_structure_array_and_string("", array[index], step+1)
+	end
       end
-      return array.join('*'+'+'*step+'*')
+      return string.join('*'+'+'*step+'*')
     end
+  end
+
+  def find_structure_indexes(option)
+    create_index_array=[]
+    update_index_array=[]
+    option[:transite_index]="" if option[:transite_index].blank?
+    option[:structure].each_with_index{|item, index|
+      next if index == 0
+      if is_string(item) == true
+	if item =~ /^update_(.*)$/
+	  update_index_array << option[:transite_index]+'['+index.to_s+']'
+	else
+	  next
+	end
+      end
+      if item[0] =~ /^\d+$/
+	create_index_array << option[:transite_index]+'['+index.to_s+']'
+      elsif item[0] =~ /^update_(.*)$/
+	update_index_array << option[:transite_index]+'['+index.to_s+']'
+      end
+      temp = find_structure_indexes(:structure=>item, :transite_index=>option[:transite_index]+'['+index.to_s+']')
+      create_index_array.concat(temp[0])
+      update_index_array.concat(temp[1])
+    }
+    return create_index_array, update_index_array
   end
 end
