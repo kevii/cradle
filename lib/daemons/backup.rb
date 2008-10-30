df76877c79d6da7790ac9d38848fff22d8417dee
jia-l <jia-l@b8adeac4-d2c7-4471-b17a-50071c40be99>
2008-10-30 04:10:25 +0000

    backup daemon bug fired
    git-svn-id: http://dahlia.naist.jp/svn-repo/Cradle/trunk@47 b8adeac4-d2c7-4471-b17a-50071c40be99

--- lib/daemons/backup.rb
+++ lib/daemons/backup.rb
@@ -17,8 +17,8 @@ backup_copies = 10     ## the number of copies
 sleep_time = 600        ## in second
 time_suffix_type = 'day'  ## time suffix type:  second or day
 
-
 last_time = 0
+
 while($running) do
   now_time = Time.now.to_s(:db).split(/-|\s|:/).join('').to_i
   if now_time-last_time>backup_interval and Time.now.hour==backup_time
@@ -43,7 +43,11 @@ while($running) do
       FileUtils.rm(all_file_array.last[0]) if all_file_array.size> backup_copies-1
       `mysqldump --opt -ujia-l -prails #{db_name} > #{dump_path_prefix+sub_dir+'/'+db_name+'-'+backup_time_suffix+'.sql'}`
     }
-    last_time = now_time
+    if time_suffix_type == "second"
+      last_time = (backup_time_suffix.slice(0..7) + "030000").to_i
+    elsif time_suffix_type == "day"
+      last_time = (backup_time_suffix + "030000").to_i
+    end
   end
   sleep sleep_time
 end
